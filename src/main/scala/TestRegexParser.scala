688dfb640269b83c6396e7a97ed3625133acd5a1
tom <tom@tomwilson.name>
2010-03-24 21:13:31 +1000

    cleaned up parser

--- src/main/scala/TestRegexParser.scala
+++ src/main/scala/TestRegexParser.scala
@@ -2,15 +2,15 @@ import util.parsing.combinator.RegexParsers
 
 object TestRegexParser extends RegexParsers {
 
-  def number = regex("[0-9]".r)
-  def direction = regex("[NSEW]".r)
-  def char = regex("[A-Z]".r)
+  def numberLiteral = regex("[0-9]".r)
+  def directionLiteral = regex("[NSEW]".r)
+  def commandLiteral = regex("[MLR]".r)
 
-  def command:Parser[Command] = char ^^ { case c => Command(c) }
-  def rover:Parser[Rover] = number ~ number ~ direction ^^ { case x ~ y ~ d => Rover(x.toInt, y.toInt, d) }
-  def commands:Parser[List[Command]] = rep(command)
-  def plateau:Parser[Plateau] = number ~ number ^^ { case x ~ y => Plateau(x.toInt, y.toInt) }
+  def command:Parser[Command] = commandLiteral ^^ { case c => Command(c) }
+  def rover:Parser[Rover] = numberLiteral ~ numberLiteral ~ directionLiteral ^^ { case x ~ y ~ d => Rover(x.toInt, y.toInt, d) }
+  def plateau:Parser[Plateau] = numberLiteral ~ numberLiteral ^^ { case x ~ y => Plateau(x.toInt, y.toInt) }
 
+  def commands:Parser[List[Command]] = (command *)
   def item = rover ~ commands
   def items = plateau ~ (item *)
 
