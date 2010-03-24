3785e471a9044b901ca12109e3a478a8754377d5
duana <strongcoffee@gmail.com>
2010-03-25 01:13:43 +1000

    get TestRegexParser working with tests

--- src/main/scala/TestRegexParser.scala
+++ src/main/scala/TestRegexParser.scala
@@ -2,6 +2,7 @@ import util.parsing.combinator.RegexParsers
 
 object TestRegexParser extends RegexParsers {
 
+  def space = regex("[ \\n]+".r)
   def numberLiteral = regex("[0-9]".r)
   def directionLiteral = regex("[NSEW]".r)
   def commandLiteral = regex("[MLR]".r)
@@ -10,10 +11,17 @@ object TestRegexParser extends RegexParsers {
   def rover:Parser[Rover] = numberLiteral ~ numberLiteral ~ directionLiteral ~ commands ^^ { case x ~ y ~ d ~ c => Rover(x.toInt, y.toInt, d, c) }
   def plateau:Parser[Plateau] = numberLiteral ~ numberLiteral ^^ { case x ~ y => Plateau(x.toInt, y.toInt) }
 
-  def commands:Parser[List[Command]] = (command *)
-//  def item = rover ~ commands
-//  def items = plateau ~ (rover *)
-  def items = plateau ~ (rover *)
 
-  def parseItem(str: String) = parse(items, str)
+  def commands:Parser[List[Command]] = (command *)
+  def rovers:Parser[List[Rover]] = (rover *)
+
+  def mission:Parser[Mission] = plateau ~ rovers ^^ {case p ~ r  => Mission(p,r)}
+
+
+  def parseMission(s:String): Mission = {
+    parse(mission, s) match {
+      case Success(result, _) => result
+      case _ => throw new IllegalArgumentException("Bad syntax: " + s)
+    }
+  }
 }
--- src/test/scala/TestRegexParserSpec.scala
+++ src/test/scala/TestRegexParserSpec.scala
@@ -2,26 +2,55 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.Spec
-import org.specs.Specification
 import org.scalatest.mock.MockitoSugar
-import org.mockito.Mockito._
-import scala.util.parsing.combinator.syntactical._
 import util.parsing.combinator.Parsers
 
 
 @RunWith(classOf[JUnitRunner])
 class TestRegexParserSpec extends Spec with ShouldMatchers with MockitoSugar with Parsers {
 
-  describe("test regex parser should") {
-    it("parse stuff") {
-      val junk = """5 6
-1 2 N
-MLMMR"""
-      val parser = (TestRegexParser)
-      val plateau = parser.parseItem(junk)
+  describe("test regex parser") {
 
-      print(plateau)
-//      plateau.map { n => print(n) }
+    it("should parse a mission with only a plateau") {
+
+      (TestRegexParser).parseMission("5 6") should equal(Mission(Plateau(5, 6), List[Rover]()))
+    }
+
+    it("should parse a mission with a plateau and one rover") {
+
+      val spec = """5 6
+                   |1 1 E
+                   |ML""".stripMargin
+
+      (TestRegexParser).parseMission(spec) should equal(Mission(Plateau(5, 6), List(Rover(1, 1, "E",
+        List(Command("M"), Command("L"))))))
+
+    }
+
+
+    it("should parse a mission with a plateau and two rovers") {
+
+      val spec = """5 6
+                   |1 1 E
+                   |M
+                   |3 2 W
+                   |L R""".stripMargin
+
+      (TestRegexParser).parseMission(spec) should equal(Mission(Plateau(5, 6),
+        List(Rover(1, 1, "E", List(Command("M"))),
+          Rover(3, 2, "W", List(Command("L"), Command("R"))))))
+
+    }
+
+    it("should not accept junk") {
+
+      val caught = intercept[IllegalArgumentException] {
+        (TestRegexParser).parseMission("tom's work")
+      }
+      caught.getMessage should be ("Bad syntax: tom's work")
+
     }
   }
+
+
 }
