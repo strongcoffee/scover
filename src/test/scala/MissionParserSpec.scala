c77b65ae52c5a96c0a6c349263ac68224a5f4738
tom <tom@tomwilson.name>
2010-03-25 19:54:04 +1000

    adding spec for MissionParser

---
+++ src/test/scala/MissionParserSpec.scala
@@ -0,0 +1,83 @@
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Spec
+import util.parsing.combinator.Parsers
+
+
+@RunWith(classOf[JUnitRunner])
+class MissionParserSpec extends Spec with ShouldMatchers with Parsers {
+  describe("mission parser should") {
+    it("parse a move command") {
+      val s = "M"
+      val parser = new MissionParser
+      val command: Command = parser.parse(parser.command, s).get
+
+      command.command should be (s)
+    }
+
+    it("parse a left command") {
+      val s = "L"
+      val parser = new MissionParser
+      val command: Command = parser.parse(parser.command, s).get
+
+      command.command should be (s)
+    }
+
+    it("parse a right command") {
+      val s = "R"
+      val parser = new MissionParser
+      val command: Command = parser.parse(parser.command, s).get
+      
+      command.command should be (s)
+    }
+
+    it ("parse multiple commands") {
+      val s = "MLR"
+      val parser = new MissionParser
+      val commands: List[Command] = parser.parse(parser.commands, s).get
+
+      commands.size should be (3)
+      commands.contains(Command("M")) should be (true)
+      commands.contains(Command("L")) should be (true)
+      commands.contains(Command("R")) should be (true)
+    }
+
+    it ("parse a plateau") {
+      val s = "2 3"
+      val parser = new MissionParser
+      val plateau: Plateau = parser.parse(parser.plateau, s).get
+
+      plateau.x should be (2)
+      plateau.y should be (3)
+    }
+
+    it ("parse a rover") {
+      val s = "1 2 N\nLMR"
+      val parser = new MissionParser
+      val rover: Rover = parser.parse(parser.rover, s).get
+
+      rover.x should be (1)
+      rover.y should be (2)
+      rover.direction should be ("N")
+
+      val commands = rover.commands
+      commands.size should be (3)
+      commands.contains(Command("M")) should be (true)
+      commands.contains(Command("L")) should be (true)
+      commands.contains(Command("R")) should be (true)
+    }
+
+    it("throw runtime exception when presented with unknown command") {
+      val s = "C"
+      val parser = new MissionParser
+
+      try {
+        parser.parse(parser.command, s)
+      } catch {
+        case e: RuntimeException => e should be ("No result when parsing failed")
+        case _ => fail
+      }
+    }
+  }
+}
