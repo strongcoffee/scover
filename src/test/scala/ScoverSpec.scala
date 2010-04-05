e88e19d3f29f53c502e6978917e1f4c6e9a6bde1
duana <strongcoffee@gmail.com>
2010-04-05 14:06:57 +1000

    make ScoverSpec more comprehensive

--- src/test/scala/ScoverSpec.scala
+++ src/test/scala/ScoverSpec.scala
@@ -11,16 +11,29 @@ class ScoverSpec extends Spec with ShouldMatchers with MockitoSugar {
   describe("scover") {
     it("should turn left") {
       Scover(1, 1, "N", List(Command("L"))).run should be (1,1,"W")
+      Scover(1, 1, "E", List(Command("L"))).run should be (1,1,"N")
+      Scover(1, 1, "S", List(Command("L"))).run should be (1,1,"E")
+      Scover(1, 1, "W", List(Command("L"))).run should be (1,1,"S")
+      //given {startPosIs => (1,1,"N")} and {commandIs => Command("L")} when scoverIsRun then "endPos" should be (1,1,"W")
     }
 
     it("should turn right") {
       Scover(1, 1, "N", List(Command("R"))).run should be (1,1,"E")
+      Scover(1, 1, "E", List(Command("R"))).run should be (1,1,"S")
+      Scover(1, 1, "S", List(Command("R"))).run should be (1,1,"W")
+      Scover(1, 1, "W", List(Command("R"))).run should be (1,1,"N")
     }
 
     it("should move") {
       Scover(0, 1, "N", List(Command("M"))).run should be (0,2,"N")
+      Scover(0, 1, "E", List(Command("M"))).run should be (1,1,"E")
+      Scover(0, 1, "S", List(Command("M"))).run should be (0,0,"S")
+      Scover(0, 1, "W", List(Command("M"))).run should be (-1,1,"W")
 
     }
-    
+
+
   }
+
+  
 }
