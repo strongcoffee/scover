aec57306429368b34d838abd42def325b3c6e3e1
duana <strongcoffee@gmail.com>
2010-04-05 11:27:27 +1000

    make ScoverSpec more correct

--- src/test/scala/ScoverSpec.scala
+++ src/test/scala/ScoverSpec.scala
@@ -10,30 +10,17 @@ class ScoverSpec extends Spec with ShouldMatchers with MockitoSugar {
 
   describe("scover") {
     it("should turn left") {
-      val scover = Scover(1, 1, "N", List(Command("L")))
-      scover.run
-
-      scover.direction should be ("W")
+      Scover(1, 1, "N", List(Command("L"))).run should be (1,1,"W")
     }
 
     it("should turn right") {
-      val scover = Scover(1, 1, "N", List(Command("R")))
-      scover.run
-
-      scover.direction should be ("E")
+      Scover(1, 1, "N", List(Command("R"))).run should be (1,1,"E")
     }
 
     it("should move") {
-      val scover = Scover(0, 1, "N", List(Command("M")))
-      scover.run
+      Scover(0, 1, "N", List(Command("M"))).run should be (0,2,"N")
 
-      scover.x should be (0)
-      scover.y should be (1)
     }
-
-    it("should return (0, 0, E)") {
-      val scover = Scover(0, 1, "N", List(Command("M")))
-      scover.run should be ((0, 0, "E"))
-    }
+    
   }
 }
