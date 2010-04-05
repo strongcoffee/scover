c28e522d463f30d70f60aac807042bba969772bb
duana <strongcoffee@gmail.com>
2010-04-05 14:12:36 +1000

    make ScoverSpec pass

--- src/main/scala/Scover.scala
+++ src/main/scala/Scover.scala
@@ -1,11 +1,26 @@
 case class Scover(var x: Int, var y: Int, var direction: String, commands: List[Command]) {
   def run: (Int, Int, String) = {
     for (c: Command <- commands) c match {
-      case Command("L") => direction = "W"
-      case Command("R") => direction = "E"
-      case Command("M") => y = 1
+      case Command("L") => direction match {
+          case "N" => direction = "W"
+          case "E" => direction = "N"
+          case "S" => direction = "E"
+          case "W" => direction = "S"
+        }
+      case Command("R") => direction match {
+          case "N" => direction = "E"
+          case "E" => direction = "S"
+          case "S" => direction = "W"
+          case "W" => direction = "N"
+        }
+      case Command("M") => direction match {
+          case "N" => y+=1
+          case "E" => x+=1
+          case "S" => y-=1
+          case "W" => x-=1
+        }
     }
 
-    (0, 0, "E")
+    (x, y, direction)
   }
 }
